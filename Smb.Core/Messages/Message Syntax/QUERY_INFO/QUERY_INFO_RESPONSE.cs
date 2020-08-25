3642f2e2903c6dfe6a503ccff0cee72fd0a08369
Shlomy Tourjeman <shlomy@aritmix.com>
2020-08-26 02:41:51 +0300

    add QUERY_INFO Request

--- Smb.Core/Messages/Message Syntax/QUERY_INFO/QUERY_INFO_RESPONSE.cs
+++ Smb.Core/Messages/Message Syntax/QUERY_INFO/QUERY_INFO_RESPONSE.cs
@@ -8,7 +8,7 @@ namespace Smb.Core
     /// The SMB2 LOGOFF Response packet is sent by the server to confirm that an SMB2 LOGOFF Request (section 2.2.7) was completed successfully. This response is composed of an SMB2 header, as specified in section 2.2.1, followed by this request structure:
     /// </summary>
     [StructLayout(layoutKind: LayoutKind.Sequential)]
-    internal class ECHO_RESPONSE
+    internal class QUERY_INFO_RESPONSE
     {
         /// <summary>
         /// </summary>
